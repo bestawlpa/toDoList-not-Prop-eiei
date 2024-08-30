f746ac48423824d4447665cab31627bc60b041d4
apiwat <bunderscorest@gmail.com>
2024-08-30 19:05:53 +0700

    add-version

--- src/component/Content.jsx
+++ src/component/Content.jsx
@@ -97,77 +97,176 @@
 // export default Content;
 
 // -- (2)
+// import React, { useState } from "react";
+
+// const content = () => {
+//   const [todos, setTodos] = useState([]);
+//   const [text, setText] = useState("");
+//   const [editText, EditSetText] = useState("");
+
+//   const addTodoHandle = () => {
+//     if (text.trim() === "") {
+//       alert("พิมพ์อะไรสักหน่อยไหม ?");
+//       return;
+//     }
+
+//     setTodos([...todos, { text, status: "complete" }]);
+//     setText("");
+//   };
+
+//   const editHandle = (index) => {
+//     const todo = todos[index];
+
+//     if (todo) {
+//       todo.status = "edit";
+//       EditSetText(todo.text);
+//       setTodos([...todos]);
+//     }
+//   };
+
+//   const saveHandle = (index) => {
+//     setTodos(
+//       todos.map((todo, i) =>
+//         i === index ? { ...todo, text: editText, status: "complete" } : todo
+//       )
+//     );
+//     EditSetText("");
+//   };
+
+//   const removeHandle = (index) => {
+//     setTodos(todos.filter((todo, i) => i !== index));
+//   };
+
+//   return (
+//     <div>
+//       <div>
+//         <h1>ToDo____List</h1>
+//         <div>
+//           <input
+//             type="text"
+//             value={text}
+//             placeholder="add"
+//             onChange={(e) => setText(e.target.value)}
+//           />
+//           <button onClick={addTodoHandle}>add si wa</button>
+//         </div>
+//       </div>
+
+//       <div className=" bg-yellow-200">
+//         {todos.map((todo, i) =>
+//           todo.status === "complete" ? (
+//             <div key={i}>
+//               <h1>{todo.text}</h1>
+//               <button onClick={() => editHandle(i)}>edit</button>
+//               <button onClick={() => removeHandle(i)}>remove</button>
+//             </div>
+//           ) : (
+//             <div key={i}>
+//               <input
+//                 type="text"
+//                 value={editText}
+//                 onChange={(e) => EditSetText(e.target.value)}
+//               />
+//               <button onClick={() => saveHandle(i)}>save</button>
+//             </div>
+//           )
+//         )}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default content;
+
 import React, { useState } from "react";
 
 const content = () => {
   const [todos, setTodos] = useState([]);
+  const [title, setTitle] = useState("");
   const [text, setText] = useState("");
-  const [editText, EditSetText] = useState("");
+  const [edittitle, editSetTitle] = useState("");
+  const [edittext, editSetText] = useState("");
 
   const addTodoHandle = () => {
-    if (text.trim() === "") {
-      alert("พิมพ์อะไรสักหน่อยไหม ?");
-      return;
-    }
+    // if (title.trim() == "" || text.trim() == "") {
+    //   alert("มึงลืมใส่อะไร ?");
+    //   return;
+    // }
 
-    setTodos([...todos, { text, status: "complete" }]);
+    setTodos([...todos, { title, text, status: "complete" }]);
+    setTitle("");
     setText("");
   };
 
-  const editHandle = (index) => {
+  const removeTodoHandle = (index) => {
+    setTodos(todos.filter((todo, i) => i !== index));
+  };
+
+  const editTodoHandle = (index) => {
     const todo = todos[index];
 
     if (todo) {
       todo.status = "edit";
-      EditSetText(todo.text);
+      editSetTitle(todo.title);
+      editSetText(todo.text);
       setTodos([...todos]);
     }
   };
 
-  const saveHandle = (index) => {
+  const saveTodoHandle = (index) => {
     setTodos(
       todos.map((todo, i) =>
-        i === index ? { ...todo, text: editText, status: "complete" } : todo
+        i === index
+          ? { ...todo, title: edittitle, text: edittext, status: "complete" }
+          : todo
       )
     );
-    EditSetText("");
-  };
-
-  const removeHandle = (index) => {
-    setTodos(todos.filter((todo, i) => i !== index));
+    editSetTitle("");
+    editSetText("");
   };
 
   return (
     <div>
       <div>
-        <h1>ToDo____List</h1>
+        <h1>ToDo___List</h1>
         <div>
           <input
             type="text"
+            placeholder="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="text"
             value={text}
-            placeholder="add"
             onChange={(e) => setText(e.target.value)}
           />
-          <button onClick={addTodoHandle}>add si wa</button>
+          <button onClick={addTodoHandle}>add</button>
         </div>
       </div>
-
-      <div className=" bg-yellow-200">
+      <div>
         {todos.map((todo, i) =>
-          todo.status === "complete" ? (
+          todo.status == "complete" ? (
             <div key={i}>
+              <h1>{todo.title}</h1>
               <h1>{todo.text}</h1>
-              <button onClick={() => editHandle(i)}>edit</button>
-              <button onClick={() => removeHandle(i)}>remove</button>
+              <button onClick={() => editTodoHandle(i)}>edit</button>
+              <button onClick={() => removeTodoHandle(i)}>remove</button>
             </div>
           ) : (
             <div key={i}>
               <input
                 type="text"
-                value={editText}
-                onChange={(e) => EditSetText(e.target.value)}
+                value={edittitle}
+                onChange={(e) => editSetTitle(e.target.value)}
               />
-              <button onClick={() => saveHandle(i)}>save</button>
+              <input
+                type="text"
+                value={edittext}
+                onChange={(e) => editSetText(e.target.value)}
+              />
+              <button onClick={() => saveTodoHandle(i)}>save</button>
             </div>
           )
         )}
