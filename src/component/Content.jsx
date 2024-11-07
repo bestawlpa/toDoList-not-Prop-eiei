8b3447038d33f4b4772d9c8d2523aec4fd3444b0
apiwat <bunderscorest@gmail.com>
2024-11-07 21:37:22 +0700

    edit

--- src/component/Content.jsx
+++ src/component/Content.jsx
@@ -4,13 +4,24 @@ const Content = () => {
   const [todos, setTodos] = useState([]);
   const [title, setTitle] = useState("");
   const [text, setText] = useState("");
-
-  const [clickComplete, setClickComplete] = useState(null);
+  const [err, setErr] = useState("");
 
   const handleAddTodo = () => {
+    if (title === "" || text === "") {
+      setErr("ลืมใส่ช่องไหนป่าวเตง !!");
+      setTimeout(() => {
+        setErr("");
+      }, [1000]);
+      return;
+    }
     setTodos([
       ...todos,
-      { id: Date.now(), title, text, status: "complete", isEditing: false },
+      {
+        id: Date.now(),
+        title: title,
+        text: text,
+        isEditing: false,
+      },
     ]);
     setTitle("");
     setText("");
@@ -21,29 +32,30 @@ const Content = () => {
   };
 
   const handleEdit = (id) => {
-    setTodos(todos.map((e) => (e.id === id ? { ...e, isEditing: true } : e)));
+    setTodos(todos.map((e) => (e.id == id ? { ...e, isEditing: true } : e)));
   };
 
-  const handleSave = (id, newTitle, newText) => {
+  const handleSave = (id, updateTilte, updatetext) => {
     setTodos(
       todos.map((e) =>
-        e.id === id
+        e.id == id
           ? {
               ...e,
-              title: newTitle,
-              text: newText,
-              status: "complete",
+              title: updateTilte,
+              text: updatetext,
               isEditing: false,
             }
           : e
       )
     );
   };
+
   return (
     <div>
       <div id="form-add">
         <h1>ToDo...List</h1>
-        <div>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          {err}
           <input
             type="text"
             placeholder="title"
@@ -58,68 +70,54 @@ const Content = () => {
           />
           <button onClick={handleAddTodo}>add</button>
         </div>
-
-        <div id="form-list">
-          <h1>List</h1>
-          <div>
-            {todos.map((e) =>
-              e.isEditing ? (
-                <div>
-                  <h1>Edit</h1>
-                  <div key={e.id}>
-                    <input
-                      type="text"
-                      value={e.title}
-                      onChange={(even) =>
-                        setTodos(
-                          todos.map((todo) =>
-                            todo.id == e.id
-                              ? {
-                                  ...todo,
-                                  title: even.target.value,
-                                }
-                              : todo
-                          )
-                        )
-                      }
-                    />
-                    <input
-                      type="text"
-                      value={e.text}
-                      onChange={(even) =>
-                        setTodos(
-                          todos.map((todo) =>
-                            todo.id == e.id
-                              ? {
-                                  ...todo,
-                                  text: even.target.value,
-                                }
-                              : todo
-                          )
-                        )
-                      }
-                    />
-                    <button onClick={() => handleSave(e.id, e.title, e.text)}>
-                      save
-                    </button>
-                  </div>
-                </div>
-              ) : (
-                <div key={e.id}>
-                  <button>complete</button>
-                  <h2>{e.title}</h2>
-                  <h5>{e.text}</h5>
-                  <div>
-                    <button onClick={() => handleEdit(e.id)}>edit</button>
-                    <button onClick={() => handleRemove(e.id)}>remove</button>
-                  </div>
-                </div>
-              )
-            )}
-          </div>
-        </div>
       </div>
-      <div></div>
+      {/*  */}
+      <div>
+        {todos.map((e) =>
+          e.isEditing ? (
+            <div key={e.id}>
+              <input
+                type="text"
+                value={e.title}
+                onChange={(update) =>
+                  setTodos(
+                    todos.map((todo) =>
+                      todo.id == e.id
+                        ? { ...todo, title: update.target.value }
+                        : todo
+                    )
+                  )
+                }
+              />
+              <input
+                type="text"
+                value={e.text}
+                onChange={(update) =>
+                  setTodos(
+                    todos.map((todo) =>
+                      todo.id == e.id
+                        ? { ...todo, text: update.target.value }
+                        : todo
+                    )
+                  )
+                }
+              />
+              <button onClick={() => handleSave(e.id, e.title, e.text)}>
+                save
+              </button>
+            </div>
+          ) : (
+            <div key={e.id}>
+              <h1>{e.title}</h1>
+              <h1>{e.text}</h1>
+              <div>
+                <button onClick={() => handleRemove(e.id)}>remove</button>
+                <button onClick={() => handleEdit(e.id)}>edit</button>
+              </div>
+            </div>
+          )
+        )}
+      </div>
     </div>
   );
 };
